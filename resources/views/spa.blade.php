da00ca3ad5a0f59d170a4cc955cfa4ae67e4267d
Dumuch <64353295+Dumuch@users.noreply.github.com>
2022-01-27 16:52:35 +0800

    add resources

---
+++ resources/views/spa.blade.php
@@ -0,0 +1,18 @@
+
+    <!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>123</title>
+
+</head>
+<body>
+<div id="app"></div>
+
+<script>
+</script>
+
+</body>
+</html>
